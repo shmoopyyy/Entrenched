1fd183363ed77137f17f077fec07a537f715316b
Alec Bargher <abargher@uchicago.edu>
2024-01-13 17:08:11 -0600

    more script work

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -7,6 +7,7 @@ public class MainCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+      
         
     }
 
---
+++ Assets/Scripts/Passerby.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passerby : MonoBehaviour
+{
+  [SerializeField]
+  private Renderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+      // spriteRenderer.sprite = 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

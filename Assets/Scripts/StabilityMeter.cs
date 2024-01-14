0ebde06924a04dd99738ec96c04c317e64eb3e01
Shayla Beltran <sbeltran@uchicago.edu>
2024-01-14 16:44:34 -0600

    stability meter take 2

--- Assets/Scripts/StabilityMeter.cs
+++ Assets/Scripts/StabilityMeter.cs
@@ -6,15 +6,19 @@ using UnityEngine.UI;
 public class StabilityMeter : MonoBehaviour
 {
     public Slider slider;
-
+    public Gradient gradient;
+    public Image fill;
 
     public void SetMaxStability(int stability)
     {
         slider.maxValue = stability;
         slider.value = stability;
+
+        fill.color = gradient.Evaluate(1f);
     }
     public void SetStability(int stability)
     {
         slider.value = stability;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }

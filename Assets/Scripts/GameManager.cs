11a6b3af4dd04d685bcff68491c402c712d225ab
Alec Bargher <abargher@uchicago.edu>
2024-01-13 18:26:24 -0600

    alec: spacing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
   public static GameManager instance;
-    void Awake()
-    {
-      if(instance == null) {
-        instance = this
-      }; 
-    }
+  void Awake()
+  {
+    if(instance == null) {
+      instance = this
+    }; 
+  }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+      
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
+      
+  }
 }

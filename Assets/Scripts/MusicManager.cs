eb3cd93da0448dc7a957fe361dd893b18f049808
Alec Bargher <abargher@uchicago.edu>
2024-01-15 13:51:07 -0600

    alec: Music manager working more

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+  public AudioSource mainTheme2;
+  public AudioSource muffledTheme2;
+  public AudioSource mainTheme3;
+  public AudioSource muffledTheme3;
+  public static bool twoKidMode = true;
+  public static bool muffleOn = false;
+
+  private GameManager GM;
+  // Start is called before the first frame update
+  void Start()
+  {
+    GM = GameManager.instance;
+    mainTheme2.Stop();
+    muffledTheme2.Stop();
+    mainTheme3.Stop();
+    muffledTheme3.Stop();
+
+    if (twoKidMode) {
+      mainTheme2.mute = false;
+    } else {
+      mainTheme3.mute = false;
+    }
+    mainTheme2.Play();
+    muffledTheme2.Play();
+    mainTheme3.Play();
+    muffledTheme3.Play();
+  }
+
+  void toggleMuffled() 
+  {
+    if (twoKidMode) {
+      if (!muffleOn) {
+        muffleOn = true;
+        mainTheme2.mute = true;
+        muffledTheme2.mute = false;
+      } else {
+        muffleOn = false;
+        muffledTheme2.mute = true;
+        mainTheme2.mute = false;
+      }
+    } else {
+      if (!muffleOn) {
+        muffleOn = true;
+        mainTheme3.mute = true;
+        muffledTheme3.mute = false;
+      } else {
+        muffleOn = false;
+        muffledTheme3.mute = true;
+        mainTheme3.mute = false;
+      }
+    }
+
+  }
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.M)) {
+      // swap music
+      toggleMuffled();
+    }
+      
+  }
+}

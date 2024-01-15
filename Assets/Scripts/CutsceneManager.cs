67e570ec2699c54f3af13d867945e7c79c7c8e3e
Alec Bargher <abargher@uchicago.edu>
2024-01-15 11:59:01 -0600

    alec: changes to cutscene manager

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class CutsceneManager : MonoBehaviour
 {
-  public static string panelsPath;
+  public static string panelsPath = "Cutscenes/Opening";
   public SpriteRenderer panelView;
   private Sprite[] panels;
-  private int numPanels;
+  private int numPanels = 0;
   private int currentPanel = 0;
   private GameManager GM;
 
   public void nextPanel()
   {
     Debug.Log("old panel is " + currentPanel);
-    currentPanel++;
+    currentPanel = Mathf.Min(currentPanel + 1, numPanels);
     panelView.sprite = panels[currentPanel];
     Debug.Log("current panel is now " + currentPanel);
   }
@@ -25,7 +25,9 @@ public class CutsceneManager : MonoBehaviour
     GM = GameManager.instance;
     panels = Resources.LoadAll<Sprite>(panelsPath);
     numPanels = panels.Length;
-    panelView.sprite = panels[0];
+    if (numPanels > 0) {
+      panelView.sprite = panels[0];
+    }
     Debug.Log("there are " + numPanels + " panels loaded.");
 
   }
